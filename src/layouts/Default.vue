84db0d8afe5ebd5fd76a1be058d9ad8ae94d43df
DavidCouronne <couronne.david@gmail.com>
2019-02-02 14:30:03 +0100

    Add Vuetify

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,46 +1,98 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link :to="{ name: 'home' }">Gridsome</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link
-          :to="{ name: 'home' }"
-          class="nav__link">Home</g-link>
-        <g-link
-          :to="{ name: 'about' }"
-          class="nav__link">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+  <v-app>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
+    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :to="item.to"
+          :key="i"
+          :active-class="color"
+          router
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"/>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"/>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+        
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar :clipped-left="clipped" fixed app dark color="primary">
+      
+      <v-btn
+          :color="color"
+          dark
+          icon
+          @click.stop="drawer = !drawer"
+        ><v-icon>reorder</v-icon>
+        </v-btn>
+        <v-btn
+          dark
+          icon
+          router
+          exact
+          to="/"
+        ><v-icon>home</v-icon>
+        </v-btn><v-btn
+          dark
+          icon
+          router
+          exact
+          to="/maths"
+        ><v-icon>school</v-icon>
+        </v-btn>
+        <v-btn
+          dark
+          icon
+          router
+          exact
+          to="/dev"
+        ><v-icon>computer</v-icon>
+        </v-btn>
+
+      <!-- <v-toolbar-title v-text="title"/> -->
+
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-container>
+        <slot/>
+     </v-container>
+    
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019 MIT David Couronné</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
+<script>
+export default {
+  data() {
+    return {
+      activenav: 'secundary',
+      color: 'secondary',
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      isSidebarOpen: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'AcadeTech',
+      items: [
+        { icon: 'home', title: 'Accueil', to: '/' },
+        { icon: 'school', title: 'Mathématiques', to: '/maths' },
+        { icon: 'computer', title: 'Développement', to: '/dev' },
+        { icon: 'computer', title: 'Cover', to: '/cover' }
+      ]
+    }
+  },
+  
 }
-
-.layout {
-  max-width: 600px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import Vuetify from 'vuetify'
 import DefaultLayout from '~/layouts/Default.vue'
 
+import 'vuetify/dist/vuetify.min.css'
+
 export default function (Vue) {
+  Vue.use(Vuetify)
   Vue.component('Layout', DefaultLayout)
 }
