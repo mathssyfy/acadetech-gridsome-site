ac8d62fbb5bd99fdb45136f78317739ba4d84d64
DavidCouronne <couronne.david@gmail.com>
2019-02-09 11:15:36 +0100

    Format Code

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,25 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 module.exports = {
-  transformers: {
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        // ...global plugins
-      ]
-    }
-  },
   plugins: [
     {
-      use: '@gridsome/source-contentful',
+      use: "@gridsome/source-contentful",
       options: {
         space: process.env.CTF_SPACE_ID, // required
         accessToken: process.env.CTF_CDA_ACCESS_TOKEN, // required
-        host: 'cdn.contentful.com',
-        environment: 'master',
-        typeName: 'Contentful'
+        host: "cdn.contentful.com",
+        environment: "master",
+        typeName: "Contentful"
       }
     }
   ]
-}
+};
---
+++ src/components/Posts/PostResponsiveImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout>
+    <v-responsive :aspect-ratio="2/1">
+      <v-img class="white--text" :src="$page.post.heroImage.file.url.src">
+        <v-container fill-height fluid>
+          <v-layout fill-height>
+            <v-flex xs12 align-end flexbox>
+              <h2 class="headline" style="text-shadow: 0px 2px 5px #222;">{{ $page.post.title }}</h2>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-img>
+    </v-responsive>
+    <p class="text-xs-right">
+      <a :to="$page.post.heroImage.description">img: {{ $page.post.heroImage.description }}</a>
+    </p>
+  </v-layout>
+</template>
