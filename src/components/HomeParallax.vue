1da551a6092fe5bb9725588274e1b11a1147b3a6
DavidCouronne <couronne.david@gmail.com>
2019-02-10 18:35:49 +0100

    UI: add dino logo

--- src/components/HomeParallax.vue
+++ src/components/HomeParallax.vue
@@ -2,7 +2,8 @@
   <v-card>
     <v-img :src="require('@/assets/img/art-dark-dawn.jpg')" height="600">
       <v-layout column align-center justify-center class="white--text" fill-height>
-        <img :src="require('@/assets/img/acadetech-icon.svg')" alt="Vuetify.js">
+        <img :src="require('@/assets/img/acadetech-icon.svg')" alt="AcadeTech">
+        <img :src="require('@/assets/img/dessin.svg')" height="150px" alt="Dinosaure">
 
         <h1 class="white--text mb-2 display-3 text-xs-center">AcadeTech</h1>
         <div class="subheading mb-3 text-xs-center">Powered by @mathssyfy</div>
